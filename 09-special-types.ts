8a951ed1fe2dcc4d18da1f378b7f41cbe64080d5
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-03-27 16:12:03 +0800

    Done playing and updating the 09-special-types.ts

--- 09-special-types.ts
+++ 09-special-types.ts
@@ -7,19 +7,30 @@ a = null
 let b: null | string
 
 b = null
-b = "Hi"
+b = "Hello"
 // b = 10
+console.log(a, b)
 
 // Special Type - undefined
 let c: undefined;
 
 c = undefined
 // c = null
-// c = "Hi"
+// c = 40
+console.log(c)
+
 
 let d: undefined | string
 
 d = undefined
-d = "Hi"
+d = "Final boss of Nod-Krai!"
 // d = null
 // d = 10
+
+console.log(d)
+
+let e: undefined | number
+e = undefined
+// e = null
+e = 10
+console.log(e)
