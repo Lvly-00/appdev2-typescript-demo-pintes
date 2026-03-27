82152f6bb7b514e351575b484b0f613e48d35e0e
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-03-27 15:31:43 +0800

    Done playing and updating the 04-arrays-objects.ts

--- 04-arrays-objects.ts
+++ 04-arrays-objects.ts
@@ -1,13 +1,14 @@
-let hobbies = ["Eating", "Watching"]
+let hobbies = ["Eating", "Playing", "Sleeping"]
 
-// hobbies.push(10)
+hobbies.push("Adventuring")
+console.log(hobbies)
 
 // let userList: (string | number)[];
 let userList: Array<string | number>;
 
-userList = ["Elmer", "Elmer123", 123456]
+userList = ["Lovely", "lvly", 123456]
 
-// console.log(userList)
+console.log(userList)
 
 let user: {
     name: string;
@@ -17,24 +18,38 @@ let user: {
         description: string,
         id: number
     }
+    address: {
+        city: string,
+        country: string
+    }
 } = {
-    name: "Elmer",
-    age: 31,
-    hobbies: ["Eating", "Sleeping"],
+    name: "Lovely",
+    age: 20,
+    hobbies: ["Eating", "Playing","Sleeping"],
     role: {
         description: 'admin',
         id: 1
+    },
+    address: {
+        city: 'Tokyo',
+        country: 'Japan'
     }
 }
 
+console.log(user)
+
 // Must not null or undefined
-let val: {} = "Elmer Alvarado"
+let val: {} = "Lovely Pintes"
+
+console.log(val)
 
 // Flexible Object - Record Type
-let data: Record<string, number | string>
+let data: Record<string, number | string | boolean>
 
 data = {
     1: 1,
-    name: 'Elmer'
-    // isPartTimeInstructor: true
+    name: 'Pintes',
+    isFreelancer: true
 }
+
+console.log(data)
