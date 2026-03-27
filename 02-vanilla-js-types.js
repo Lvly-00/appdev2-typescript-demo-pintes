7cb3834786c8fa81ac9c4b20aa171fe645506979
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-03-27 14:40:56 +0800

    Done playing and updating the 02-vanilla-js-types.js

--- 02-vanilla-js-types.js
+++ 02-vanilla-js-types.js
@@ -1,3 +1,10 @@
-let userName1 = "Elmer";
+let userName1 = "Lovely";
+let age = 20;
+let isAdmin = true;
+let hobbies = ["Cooking", "Sports"];
+
 
 console.log(typeof userName1)
+console.log(typeof age)
+console.log(typeof isAdmin)
+console.log(typeof hobbies)
